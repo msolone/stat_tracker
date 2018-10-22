57441e3f680d118f9c7c35aa0fe3c5dfbfecc3b5
Michael Solone <michaelsolone@gmail.com>
2018-10-22 15:17:50 -0400

    Basic Structure for StatDisplay, now work on server

--- client/src/components/StatDisplay.vue
+++ client/src/components/StatDisplay.vue
@@ -1,14 +1,29 @@
 <template>
     <div class="stat_display">
-        <h1>
-            Stat Display
-        </h1>
+        <div class="player_stats">
+            <h2>Michael Solone</h2>
+            <div>Passing Attempts: 25</div>
+            <div>Passing Completions: 25</div>
+            <div>Completion Percentage: 100%</div>
+            <div>Passing Yards: 400</div>
+            <div>Yards/Completion: 16</div>
+            <div>Passing TD: 4</div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: "StatDisplay"
+  name: "StatDisplay",
+  data: function() {
+    return {
+      player_name: "",
+      pass_attempts: 0,
+      pass_completions: 0,
+      pass_yards: 0,
+      pass_td: 0
+    };
+  }
 };
 </script>
 
@@ -20,5 +35,11 @@ export default {
   height: 100%;
   width: 100%;
   border: solid black 3px;
+}
+
+.player_stats {
+    display: flex;
+    flex-direction: column;
+
 }
 </style>
